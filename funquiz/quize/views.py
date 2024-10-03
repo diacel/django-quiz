f1654249287044c37beb80dbdee4fb8c0d21b0d6
Denis Lozhnikov <den.lozhnikov@bk.ru>
2024-10-03 20:51:48 +0500

    03.10

--- funquiz/quize/views.py
+++ funquiz/quize/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Question
 
-# Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})

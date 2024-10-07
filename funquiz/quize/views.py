bcd2fbe3089c99b422e8604410332f59ff4db8c4
Студент группы IS <is@sielom.local>
2024-10-07 17:39:17 +0500

    07.10

--- funquiz/quize/views.py
+++ funquiz/quize/views.py
@@ -4,12 +4,12 @@ from . import models
 def index(request):
     latest_question_list = models.Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    return render(request, 'index.html', context)
 
 def detail(request, question_id):
     question = models.Question.objects.get(pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    return render(request, 'detail.html', {'question': question})
 
 def results(request, question_id):
     question = models.Question.objects.get(pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+    return render(request, 'results.html', {'question': question})
